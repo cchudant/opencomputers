a377fcf2c0c4b9f94b8e9bfe5e6df7474d2e0be6
cchudant <cchudant@proton.me>
2026-04-07 04:11:30 +0200

    edit test dronescript

--- software/drone/test.lua
+++ software/drone/test.lua
@@ -1,9 +1,24 @@
-drone.moveRel(0, 5, 0)
 
-drone.moveRel(0, 0, 5)
+print("pos:", table.unpack(drone.position))
 
 drone.sleep(10)
 
-drone.moveRel(0, 0, -5)
+print("2 pos:", table.unpack(drone.position))
+
+drone.moveRel(0, 5, 0)
+
+print("0,5,0 pos:", table.unpack(drone.position))
 
 drone.moveRel(0, -5, 0)
+
+print("origin pos:", table.unpack(drone.position))
+
+drone.sleep(10)
+
+print("0,0,-5 pos:", table.unpack(drone.position))
+
+drone.moveRel(0, 0, -5)
+
+print("origin pos:", table.unpack(drone.position))
+
+drone.moveRel(0, 0, 5)

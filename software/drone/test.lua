2b1cb0c6f1edb918140fe0998d7f0fef155fdc55
cchudant <cchudant@proton.me>
2026-04-07 17:55:31 +0200

    fix drone movement tracking

--- software/drone/test.lua
+++ software/drone/test.lua
@@ -3,17 +3,22 @@ local function moveToBlock(x, y, z)
     drone.moveTo(x + .5, y + .5, z + .5)
 end
 
+print('current pos: ', table.unpack(drone.position))
 local home = drone.position
 
 print('moving to there')
 moveToBlock(572,141,-501)
+print('current pos: ', table.unpack(drone.position))
 
-drone.sleep(10)
+drone.sleep(3)
 
+print('current pos: ', table.unpack(drone.position))
 print('moving to home ', table.unpack(home))
 
 drone.moveTo(home)
 
-drone.sleep(10)
+print('current pos: ', table.unpack(drone.position))
+
+drone.sleep(3)
 
 print('done?')

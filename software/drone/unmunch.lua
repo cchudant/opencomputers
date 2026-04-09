e6ab31cd53369af54c3f106337ef9309298240ab
krivnbd <herrmannmw@gmail.com>
2026-04-08 23:46:54 -0400

    droneMuncher unmunch test

---
+++ software/drone/unmunch.lua
@@ -0,0 +1,1 @@
+drone.moveRel(-5, 0, 0)
---
+++ software/programs/droneMuncher.lua
@@ -0,0 +1,71 @@
+local droneControl = require('.software.apis.droneControl')
+local sides = require('sides')
+local robot = require('robot')
+local inv = component.inventory_controller
+local cr = component.crafting
+local modem = component.modem
+modem.setStrength(1)
+
+
+local function showUsage()
+    print('droneMuncher')
+    print('Usage:')
+    print('* droneMuncher munch - will order every waiting drone to get munched.')
+    print('* droneMuncher unmunch - will deploy drones')
+end
+
+---copy eeprom by taking flashed eeprom and crafting with an unflashed eeprom, then place both back in flashed eeprom slot
+local function copy_eeprom()
+    robot.select(15)
+    robot.transferTo(1, 1)
+    robot.select(16)
+    robot.transferTo(2, 1)
+    cr.craft(1)
+    robot.select(1)
+    robot.transferTo(15, 2)
+end
+
+---take in drone, craft with a flashed eeprom, then if doing so returned an eeprom move drone from grid, clear eeprom, move to unflashed
+---eeprom slot, then put drone back in slot 1
+local function add_eeprom()
+    robot.select(1)
+    inv.suckFromSlot(sides.down, 1, 1)
+    robot.select(15)
+    robot.transferTo(2, 1)
+    cr.craft(1)
+    if inv.getStackInInternalSlot(2) ~= nil then
+        robot.select(1)
+        robot.transferTo(13, 1)
+        cr.craft(1)
+        robot.transferTo(16, 1)
+        robot.select(13)
+        robot.transferTo(1)
+    end
+end
+
+--place and activate drone, then send signal for drones nearby to move to waiting area
+local function deploy()
+    robot.select(1)
+    robot.place(sides.front)
+    robot.use(sides.front, true)
+    droneControl.run("/software/drone/unmunch.lua")
+end
+
+local args = ...
+
+if args == 'munch' then
+    
+elseif args == 'unmunch' then
+    while inv.getStackInSlot(sides.down, 1) do
+        if inv.getStackInInternalSlot(16) ~= nil then
+            copy_eeprom()
+            add_eeprom()
+            deploy()
+        else
+            print("Out of EEPROMs!")
+            break
+        end
+    end
+else
+    showUsage()
+end

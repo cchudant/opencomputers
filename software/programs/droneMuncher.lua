891ad9e7736a2c21af605a555712c3ed107ff264
cchudant <cchudant@proton.me>
2026-04-10 19:23:52 +0200

    debugging unmunch

--- software/programs/droneMuncher.lua
+++ software/programs/droneMuncher.lua
@@ -23,39 +23,60 @@ local slotTemp = 4
 
 ---copy eeprom by taking flashed eeprom and crafting with an unflashed eeprom, then place both back in flashed eeprom slot
 local function copy_eeprom()
+    os.sleep(1)
     robot.select(slotGoodEeprom)
+    os.sleep(1)
     robot.transferTo(1, 1)
     robot.select(slotEmptyEeproms)
+    os.sleep(1)
     robot.transferTo(2, 1)
+    os.sleep(1)
     cr.craft(1)
+    os.sleep(1)
     robot.select(1)
+    os.sleep(1)
     robot.transferTo(slotGoodEeprom, 2)
 end
 
 ---take in drone, craft with a flashed eeprom, then if doing so returned an eeprom move drone from grid, clear eeprom, move to unflashed
 ---eeprom slot, then put drone back in slot 1
 local function add_eeprom()
+    os.sleep(1)
     robot.select(1)
+    os.sleep(1)
     inv.suckFromSlot(sides.down, 1, 1)
+    os.sleep(1)
     robot.select(slotGoodEeprom)
+    os.sleep(1)
     robot.transferTo(2, 1)
+    os.sleep(1)
     cr.craft(1)
 
     if inv.getStackInInternalSlot(2) ~= nil then
+        os.sleep(1)
         robot.select(1)
+        os.sleep(1)
         robot.transferTo(slotTemp, 1)
+        os.sleep(1)
         cr.craft(1)
+        os.sleep(1)
         robot.transferTo(slotEmptyEeproms, 1)
+        os.sleep(1)
         robot.select(slotTemp)
+        os.sleep(1)
         robot.transferTo(1, 1)
+        os.sleep(1)
         robot.select(1)
     end
 end
 
 --place and activate drone, then send signal for drones nearby to move to waiting area
 local function deploy()
+    os.sleep(1)
     robot.place(sides.front)
+    os.sleep(1)
     robot.use(sides.front, true)
+    os.sleep(2)
     droneControl.run(nil, '/software/drone/unmunch.lua')
 end
 
@@ -79,7 +100,7 @@ elseif args == 'unmunch' then
     component.getPrimary('modem').setStrength(2)
 
     while inv.getStackInSlot(sides.down, 1) do
-        if inv.getStackInInternalSlot(slotGoodEeprom) ~= nil then
+        if inv.getStackInInternalSlot(slotEmptyEeproms) ~= nil then
             copy_eeprom()
             add_eeprom()
             deploy()

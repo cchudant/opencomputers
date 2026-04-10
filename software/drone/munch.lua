ffae3f15cb10fe9a20b09a8d62e476d68fce05f8
cchudant <cchudant@proton.me>
2026-04-10 19:58:11 +0200

    fix droneMuncher

--- software/drone/munch.lua
+++ software/drone/munch.lua
@@ -1,12 +1,1 @@
-drone.moveRel(2, 0, 0)
-
-while not drone.gpsUpdatedAt do
-    drone.setLightColor(0xFFFFFF)
-    drone.setStatusText('GPS...')
-    drone.sleep(1)
-end
-
-
-drone.moveTo(584.5, -141.5, 489.5)
-os.sleep(10) -- in case gps wasnt accurate enough, fix
-drone.moveTo(584.5, -141.5, 489.5)
+drone.moveRel(4, 0, 0)
--- software/drone/unmunch.lua
+++ software/drone/unmunch.lua
@@ -1,1 +1,11 @@
-drone.moveTo(588.5, 141.5, -489.5)
+drone.moveRel(2, 0, 0)
+
+while not drone.gpsUpdatedAt do
+    drone.setLightColor(0xFFFFFF)
+    drone.setStatusText('GPS...')
+    drone.sleep(1)
+end
+
+local x,y,z = 584.5, -141.5, 489.5
+
+drone.moveTo(x,y,z)

272cf46c655b40e359a87be9b7631ec6b3d38140
cchudant <cchudant@proton.me>
2026-04-10 19:08:34 +0200

    debugging unmunch

--- software/programs/droneMuncher.lua
+++ software/programs/droneMuncher.lua
@@ -23,35 +23,60 @@ local slotTemp = 4
 
 ---copy eeprom by taking flashed eeprom and crafting with an unflashed eeprom, then place both back in flashed eeprom slot
 local function copy_eeprom()
+    print('copy eeprom')
+    os.sleep(3)
     robot.select(slotGoodEeprom)
+    os.sleep(3)
     robot.transferTo(1, 1)
+    os.sleep(3)
     robot.select(slotEmptyEeproms)
+    os.sleep(3)
     robot.transferTo(2, 1)
+    os.sleep(3)
     cr.craft(1)
+    os.sleep(3)
     robot.select(1)
+    os.sleep(3)
     robot.transferTo(slotGoodEeprom, 2)
 end
 
 ---take in drone, craft with a flashed eeprom, then if doing so returned an eeprom move drone from grid, clear eeprom, move to unflashed
 ---eeprom slot, then put drone back in slot 1
 local function add_eeprom()
+    print('add eeprom')
     robot.select(1)
+    os.sleep(3)
     inv.suckFromSlot(sides.down, 1, 1)
+    os.sleep(3)
     robot.select(slotGoodEeprom)
+    os.sleep(3)
     robot.transferTo(2, 1)
+    os.sleep(3)
     cr.craft(1)
 
     if inv.getStackInInternalSlot(1) ~= nil then
+        print('heya')
+        os.sleep(3)
         robot.select(1)
+        os.sleep(3)
         robot.transferTo(slotTemp, 1)
+        os.sleep(3)
         cr.craft(1)
+        os.sleep(3)
         robot.transferTo(slotEmptyEeproms, 1)
+        os.sleep(3)
+        robot.select(slotTemp)
+        robot.transferTo(1, 1)
+        robot.select(1)
     end
 end
 
 --place and activate drone, then send signal for drones nearby to move to waiting area
 local function deploy()
+    print('deploy')
+    os.sleep(3)
     robot.place(sides.front)
+    os.sleep(3)
     robot.use(sides.front, true)
     os.sleep(2)
     droneControl.run(nil, '/software/drone/unmunch.lua')

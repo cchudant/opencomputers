9fb853419b3c283fbd1c82155aa4c16371b1676d
cchudant <cchudant@proton.me>
2026-04-10 19:47:00 +0200

    debugging unmunch

--- software/drone/munch.lua
+++ software/drone/munch.lua
@@ -1,1 +1,9 @@
-drone.moveRel(5, 0, 0)
+drone.moveRel(2, 0, 0)
+
+while not drone.gpsUpdatedAt do
+    drone.setLightColor(0xFFFFFF)
+    drone.setStatusText('GPS...')
+    drone.sleep(1)
+end
+
+drone.moveTo(584.5, -141.5, 489.5)
--- software/programs/droneMuncher.lua
+++ software/programs/droneMuncher.lua
@@ -4,9 +4,9 @@ local sides = require('sides')
 local robot = require('robot')
 local inv = component.inventory_controller
 local cr = component.crafting
-local modem = component.modem
-modem.setStrength(1)
 
+local eepromItemName = 'OpenComputers:eeprom'
+local screnchItemName = 'OpenComputers:wrench'
 
 local function showUsage()
     print('droneMuncher')
@@ -23,91 +23,100 @@ local slotTemp = 4
 
 ---copy eeprom by taking flashed eeprom and crafting with an unflashed eeprom, then place both back in flashed eeprom slot
 local function copy_eeprom()
-    os.sleep(1)
+    local emptyEeproms = inv.getStackInInternalSlot(slotEmptyEeproms)
+    if not emptyEeproms or emptyEeproms.name ~= eepromItemName then
+        robot.select(slotEmptyEeproms)
+        print("Please put empty eeproms in the selected slot. (slot " .. slotEmptyEeproms .. ")")
+        os.exit(1)
+    end
+
     robot.select(slotGoodEeprom)
-    os.sleep(1)
     robot.transferTo(1, 1)
     robot.select(slotEmptyEeproms)
-    os.sleep(1)
     robot.transferTo(2, 1)
-    os.sleep(1)
     cr.craft(1)
-    os.sleep(1)
     robot.select(1)
-    os.sleep(1)
     robot.transferTo(slotGoodEeprom, 2)
 end
 
 ---take in drone, craft with a flashed eeprom, then if doing so returned an eeprom move drone from grid, clear eeprom, move to unflashed
 ---eeprom slot, then put drone back in slot 1
 local function add_eeprom()
-    os.sleep(1)
     robot.select(1)
-    os.sleep(1)
     inv.suckFromSlot(sides.down, 1, 1)
-    os.sleep(1)
     robot.select(slotGoodEeprom)
-    os.sleep(1)
     robot.transferTo(2, 1)
-    os.sleep(1)
     cr.craft(1)
-    robot.select(1)
 
+    -- In case there was already an eeprom in the drone, it will be returned to us
+    -- in slot 2.
     if inv.getStackInInternalSlot(2) ~= nil then
-        os.sleep(1)
         robot.transferTo(slotTemp, 1)
-        os.sleep(1)
         cr.craft(1)
-        os.sleep(1)
         robot.transferTo(slotEmptyEeproms, 1)
-        os.sleep(1)
         robot.select(slotTemp)
-        os.sleep(1)
         robot.transferTo(1, 1)
-        os.sleep(1)
         robot.select(1)
     end
 end
 
 --place and activate drone, then send signal for drones nearby to move to waiting area
 local function deploy()
-    os.sleep(1)
+    robot.select(1)
     robot.place(sides.front)
-    os.sleep(1)
     robot.use(sides.front, true)
-    os.sleep(5)
+    os.sleep(3)
     droneControl.run(nil, '/software/drone/unmunch.lua')
 end
 
 local args = ...
 
 if args == 'munch' then
-    inv.select(slotScrench)
+    robot.select(slotScrench)
+
+    local item = inv.getStackInInternalSlot(slotScrench)
+    if not item or item.name ~= screnchItemName then
+        print("Please put a scrench in the selected slot. (slot " .. slotScrench .. ")")
+        os.exit(1)
+    end
+
     inv.equip()
 
-    inv.select(1)
+    robot.select(1)
     while inv.use(sides.front, true) do
         inv.dropDown()
     end
 
-    inv.select(slotScrench)
+    robot.select(slotScrench)
     -- unequip
     inv.equip()
-    inv.select(1)
+    robot.select(1)
 elseif args == 'unmunch' then
     -- Messages only travel two blocks.
     component.getPrimary('modem').setStrength(2)
 
+    local deployed = 0
+
     while inv.getStackInSlot(sides.down, 1) do
-        if inv.getStackInInternalSlot(slotEmptyEeproms) ~= nil then
-            copy_eeprom()
-            add_eeprom()
-            deploy()
-        else
-            print("Out of EEPROMs!")
-            break
+
+        local goodEeproms = inv.getStackInInternalSlot(slotGoodEeprom)
+        if not goodEeproms or goodEeproms.name ~= eepromItemName then
+            robot.select(slotGoodEeprom)
+            print("Please put the eeprom to flash in the selected slot. (slot " .. slotGoodEeprom .. ")")
+            os.exit(1)
         end
+
+        if goodEeproms.size < 2 then
+            copy_eeprom()
+        end
+
+        add_eeprom()
+        deploy()
+
+        deployed = deployed + 1
     end
+
+    print('Done! Drones unmunched: ' .. deployed)
 else
     showUsage()
 end

7edd3b26e5ce6bed030e9e65de94117be15539ff
cchudant <cchudant@proton.me>
2026-04-15 23:26:46 +0200

    y15

--- software/programs/recl.lua
+++ software/programs/recl.lua
@@ -19,8 +19,8 @@ end
 
 local action = 'deploy'
 
-local schemFilePath = '/home/reclamation5_y14.data'
-local schemX, schemY, schemZ = 192, 14, -880
+local schemFilePath = '/home/reclamation5_y15.data'
+local schemX, schemY, schemZ = 192, 15, -880
 
 if type(ops['checkMatList']) == 'boolean' then
     action = 'checkMatList'

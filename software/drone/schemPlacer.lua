c205f182f58b0fa8c540852d431601da177bb6d3
cchudant <cchudant@proton.me>
2026-04-15 03:09:19 +0200

    idk......?

--- software/drone/schemPlacer.lua
+++ software/drone/schemPlacer.lua
@@ -31,7 +31,7 @@ local sides = {
 local function dump()
     for i = 1, drone.tankCount() do
         local tankinfo = tank_controller.getFluidInInternalTank(i)
-        if level > 0 then
+        if tankinfo.amount > 0 then
             drone.selectTank(i)
             drone.fill(sides.posz, tankinfo.amount)
         end
